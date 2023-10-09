b2e6088ae54bb043dcd99f6821bf701bf0ffa06d
TangLei <907699404@qq.com>
2023-10-09 22:26:47 +0800

    Update DaNs_DE_genes.R

--- code/DaNs_DE_genes.R
+++ code/DaNs_DE_genes.R
@@ -3,11 +3,11 @@ suppressPackageStartupMessages(library(SeuratData))
 suppressPackageStartupMessages(library(SeuratDisk))
 suppressPackageStartupMessages(library(dplyr))
 library(ggplot2)
-Convert('./stored each_subcluster h5 files/dans_umap.h5ad',dest = "h5seurat", overwrite = TRUE)
-ad_dans <- LoadH5Seurat('./stored each_subcluster h5 files/dans_umap.h5seurat',assays="RNA")
-for (i in unique(ad_dans$label_9)){
-    ad_temp <- subset(ad_dans,label_9==i)
-    Idents(ad_temp) <-'exp_condition1'
+Convert('../data/dans_umap.h5ad',dest = "h5seurat", overwrite = TRUE)
+ad_dans <- LoadH5Seurat('../data/dans_umap.h5seurat',assays="RNA")
+for (i in unique(ad_dans$label_subtype)){
+    ad_temp <- subset(ad_dans,label_subtype==i)
+    Idents(ad_temp) <-'Condition'
     all_markers <- FindAllMarkers(ad_temp, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
     write.csv(all_markers,file=paste0('./de_genes/',i,'.csv'))
 }
